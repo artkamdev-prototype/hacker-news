a262734a2b4dafc5f3a9cfa9cad0c05651060349
Ognjen Koldzic <ognjen_koldzic@yahoo.de>
2022-06-02 14:09:36 +0200

    WIP:firstFetchRendered

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import List from "./components/List"
+import List from "./components/List";
+import { useState, useEffect } from "react";
 
 function App() {
   // logic to get the username
-  const username = "John Doe";
+  const [posts, setPosts] = useState([]);
+  const url = "http://hn.algolia.com/api/v1/search_by_date";
+  const [tag, setTag] = "x";
+  const urlSearch = `http://hn.algolia.com/api/v1/search?query=${tag}`;
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        // console.log(response.json());
+        return response.ok && response.json();
+      })
+
+      .then((json) => setPosts(json.hits))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(posts);
 
   return (
     <div className="App">
-      <Navbar username={username} />
-      <h2>Our List:</h2>
-      <List />
+      {posts.map((post) => (
+        <div>
+          <span>{post.author}</span>
+          <br />
+          <span>{post.created_at}</span>
+          <p> {post.comment_text}</p>
+        </div>
+      ))}
     </div>
   );
 }
+//
 
 export default App;
